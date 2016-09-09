42eef70953ceeae33308ad491e586f2810716865
kmuuga <kmuuga@klass1-20-l.intra.itcollege.ee>
2016-09-09 12:10:59 +0300

    Cum Laude korras

--- src/pr2/CumLaude.java
+++ src/pr2/CumLaude.java
@@ -4,25 +4,25 @@ import lib.TextIO;
 
 public class CumLaude {
 	public static void main(String[] args) {
-		
+
 		System.out.println("Tere!\nPalun sisesta enda kaalutud keskmine hinne!");
-		
+
 		double hinne = TextIO.getlnDouble();
-		while(hinne < 1 || hinne > 5)
+		while (hinne < 1 || hinne > 5) {
 			System.out.println("Palun sisesta korrektne hinne!");
 			hinne = TextIO.getlnDouble();
-		
+		}
 		System.out.println("Palun sisesta enda lõputöö hinne!");
-		
+
 		double too = TextIO.getlnDouble();
-		while(too < 1 || too > 5)
+		while (too < 1 || too > 5) {
 			System.out.println("Palun sisesta korrektne hinne!");
 			too = TextIO.getlnDouble();
-			
-		if(hinne > 4.5 && too == 5)
+		}
+		if (hinne > 4.5 && too == 5)
 			System.out.println("Õnnitlused! Sa saad Cum Laude! Aga mida sa sellega nüüd peale hakkad?");
 		else
 			System.out.println("Loodan, et sa üldse lõpetad..");
-			
+		
 	}
 }
