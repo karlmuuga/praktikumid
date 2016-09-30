2f8fb23553faf3bd7e19b80c136b212e04c8b06b
kmuuga <kmuuga@klass1-03-l.intra.itcollege.ee>
2016-09-30 12:49:29 +0300

    Liisu 7 valmis

--- src/pr7/Liisu.java
+++ src/pr7/Liisu.java
@@ -1,8 +1,20 @@
 package pr7;
 
+import lib.TextIO;
+import pr6.Methods;
+
 public class Liisu {
 	public static void main(String[] args) {
 		
+		int n = Methods.kasutajaSisestus("Palun sisestage inimeste arv!", 1, 10);
+		String[] massiiv = new String[n];
+		
+		for(int i = 0 ; i < n ; i++){
+			int b = i+1;
+			TextIO.putln("Palun sisesta inimese nr " + b + " nimi!");
+			massiiv[i] = TextIO.getlnString();
+		}
+		TextIO.putln("Tema nimi on " + massiiv[Methods.suvakas(0, (n-1))]);
 	}
 
 }
