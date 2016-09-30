3662ab4ae4201c35aa45d75f08dbb23b976c99ec
kmuuga <kmuuga@klass1-03-l.intra.itcollege.ee>
2016-09-30 12:53:05 +0300

    l

---
+++ src/pr7/Arvud.java
@@ -0,0 +1,17 @@
+package pr7;
+
+import lib.TextIO;
+
+public class Arvud {
+
+	public static void main(String[] args) {
+
+
+		for (int i = 0; i < 10; i++) {
+			TextIO.putln("Palun sisesta arv!");
+			int a = TextIO.getlnInt();
+			
+		}
+	}
+
+}
