4e0acd0d0cc1a52871e5d536eb1dc8a063dc1842
kmuuga <kmuuga@klass1-17-l.intra.itcollege.ee>
2016-10-21 13:23:49 +0300

    Third one complete

--- src/pr10/Ylesanne.java
+++ src/pr10/Ylesanne.java
@@ -1,6 +1,7 @@
 package pr10;
 
 import java.util.ArrayList;
+import java.util.Collections;
 
 import lib.TextIO;
 import pr8.Maximal;
@@ -12,12 +13,23 @@ public class Ylesanne {
 		while (!stopp) {
 			TextIO.putln("Millist ülesannet soovid näha? Lõpetamiseks vajuta enter (või midagi muud).");
 			switch (TextIO.getlnString()) {
+
 			case "1":
 				suurim();
 				break;
+
 			case "1.2":
 				neo();
 				break;
+
+			case "2":
+				dia();
+				break;
+
+			case "3":
+				dia2();
+				break;
+
 			default:
 				TextIO.putln("Nägemist!");
 				stopp = true;
@@ -32,36 +44,77 @@ public class Ylesanne {
 		TextIO.putln("Massiivi suurim väärtus on: " + Maximal.maksimum(massiiv));
 		return 1;
 	}
-	
+
 	public static int neo() {
-		TextIO.putln("Deklareerime maatriksi järgmiselt: int[][] neo = {\n{1, 3, 6, 7},\n{2, 3, 3, 1},\n{17, 4, 5, 0},\n{-20, 13, 16, 17}\n};\n");
-		int[][] maatriks = {
-			    {1, 3, 6, 7},
-			    {2, 3, 3, 1},
-			    {17, 4, 5, 0},
-			    {-20, 13, 16, 17}
-			};
+		TextIO.putln(
+				"Deklareerime maatriksi järgmiselt: int[][] neo = {\n{1, 3, 6, 7},\n{2, 3, 3, 1},\n{17, 4, 5, 0},\n{-20, 13, 16, 17}\n};\n");
+		int[][] maatriks = { { 1, 3, 6, 7 }, { 2, 3, 3, 1 }, { 17, 4, 5, 0 }, { -20, 13, 16, 17 } };
 		TextIO.putln("Maatriksi suurim väärtus on: " + Maximal.maksimum(maatriks));
 		return 1;
 	}
-	
-	public static String dia(){
+
+	public static String dia() {
 		ArrayList<Integer> arvud = new ArrayList<Integer>();
-		while(true){
-			TextIO.putln("Palun sisesta täisarv. Nulli sisestamine katkestab!");
+		TextIO.putln("Palun sisesta täisarvud enteriga. Nulli sisestamine katkestab!");
+		while (true) {
 			int s = TextIO.getlnInt();
-			if(s == 0)
+			if (s == 0)
 				break;
 			else
 				arvud.add(s);
 		}
-		int si = arvud.size();
-		int massiiv[][] = new int[arvud.size()][arvud.size()];
-		int n = 0;
-		for (Integer arv : arvud) {
-			massiiv[n][0] = 5;
+		float ratio = 1;
+		if (Collections.max(arvud) > 80) {
+			ratio = Collections.max(arvud) / 80;
+			for (int b = 0; b < arvud.size(); b++)
+				arvud.set(b, (int) (arvud.get(b) / ratio));
 		}
 
+		for (Integer arv : arvud) {
+			String graph = "";
+			for (int n = 0; n < arv; n++)
+				graph += "x";
+			System.out.format("%s %s%n", Math.round(arv * ratio), graph);
+		}
+		TextIO.putln("");
+
+		return "korras";
+	}
+
+	public static String dia2() {
+		ArrayList<Integer> arvud = new ArrayList<Integer>();
+		TextIO.putln("Palun sisesta täisarvud enteriga. Nulli sisestamine katkestab!");
+		while (true) {
+			int s = TextIO.getlnInt();
+			if (s == 0)
+				break;
+			else
+				arvud.add(s);
+		}
+		int suurim = Collections.max(arvud);
+
+		for (int rida = (suurim + 2); rida > 0; rida--) {
+
+			if (rida == 2) {
+				for (int b = 1; b <= arvud.size(); b++)
+					TextIO.put("---");
+				TextIO.putln("");
+			} else if (rida == 1) {
+				for (Integer arv : arvud)
+					System.out.format("  %s", arv);
+				TextIO.putln("");
+
+			} else {
+				for (Integer arv : arvud) {
+					if (arv >= (rida - 2))
+						TextIO.put("  x");
+					else
+						TextIO.put("   ");
+				}
+				TextIO.putln("");
+			}
+		}
+		TextIO.putln("");
 		return "korras";
 	}
 
